e95268bbd08f70cc15090afab03f1dc0efb52a64
YannBOUET <yann-dbz@hotmail.fr>
2020-02-12 15:58:54 +0100

    Methode nouveauStationnement  ET  sortieStationnement

--- src/main/java/fr/eval/Parking.java
+++ src/main/java/fr/eval/Parking.java
@@ -19,7 +19,52 @@ public class Parking {
 
     public void ajouterImmatriculationAutorisé(String immatriculation){
 
-        véhiculeAutorisé.add(immatriculation); // On ajoute dans la liste la plaque passé en param
+        if (véhiculeAutorisé.contains(immatriculation)){
+            System.out.println("Ce véhicule est déjà autorisé");
+        }
+        else{
+            véhiculeAutorisé.add(immatriculation); // On ajoute dans la liste la plaque passé en param
+            System.out.println("Votre plaque a bien été ajouté");
+        }
+        
+    }
+
+    public boolean nouveauStationnement(String immatriculation) {
+        boolean isSuccess;
+        // On vérifie qu'il reste de la place sur le parking
+        if (véhiculeStationné.size() <= maxPlace){
+            // On vérifie que le véhicule est bien autorisé à stationner
+            if (véhiculeAutorisé.contains(immatriculation)){
+                // On ajoute le véhicule sur le parking
+                véhiculeStationné.add(immatriculation); 
+
+                isSuccess = true;
+            }
+            else{
+                isSuccess = false;
+            }
+        }
+        else{
+            isSuccess = false;
+        }
+
+        return isSuccess;
     }
     
+    public boolean sortieStationnement(String immatriculation) {
+        boolean isSuccess;
+        // On vérifie que le véhicule était bien enregistré sur le parking
+        if (véhiculeStationné.contains(immatriculation)){
+            // On retire la plaque pour signifier le départ du véhicule
+            véhiculeStationné.remove(immatriculation);
+
+            isSuccess = true;
+        }
+        else{
+            System.out.println("Le véhicule n'était pas enregistré sur le parking");
+
+            isSuccess = false;
+        }
+        return isSuccess ;
+    }
 }
